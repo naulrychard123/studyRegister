fcbf3333e65691be5efe96a1830a95d1577c8078
Naul Rychard <rychardluan35@gmail.com>
2026-02-28 21:14:29 -0300

    adicionando fluxo save do model

--- api/controller.js
+++ api/controller.js
@@ -1,1 +1,20 @@
-const model = require('model')
+const model = require('./model')
+
+async function save(req, res){
+    const { conteudo, data, time, desc } = req.body
+
+    if(!conteudo || !data || !time || !desc){
+        return res.status(400).json({message: 'Campo obrigatórios faltando'})
+    }
+    try {
+        const result = await model.save(conteudo, data, time, desc)
+
+        if(result.affectedRows > 0){
+            return res.status(201).json({message: 'Sucesso ao salvar os dados'})
+        }
+    } catch (error){
+        return res.status(500).json({message: 'Houve um erro ao salvar os dados no banco', error})
+    }
+}
+
+module.exports = { save }
--- api/model.js
+++ api/model.js
@@ -39,12 +39,16 @@ async function readRegisters(param){
 Essas datas precisam ser previamente determinadas no controller para não gerar uma consulta redundante.
 Como por exemplo: as mesmas datas serem colocadas 2 vezes, ou uma data inesistente no banco
 
+Esse modelo pode ser montado no controller
 json: {
 data1: '00-00-0000',
 data2: '00-00-0000'
 } */
+
 async function returnToStudyTime(betweenData){
-    const sql = `SELECT * FROM studyRegisters BETWEEN ? AND ?;`
+    const sql = `SELECT * FROM studyRegisters WHERE data BETWEEN ? AND ?;`
+    const results = await connection.query(sql, [betweenData.data1, betweenData.data2])
+    return results
 }
 
-module.exports = {save, readRegisters, returnToStudyTime }
+module.exports = { save, readRegisters, returnToStudyTime }
